48a3a131929b27d01c2b7cb1a12c48ad699e741f
uraokayutaro1990 <69355356+uraokayutaro1990@users.noreply.github.com>
2020-09-01 15:41:31 +0900

    修正 9.1

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -35,8 +35,6 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
       wday_num = Date.today.wday + x
-      #Date.today.wday + x を利用して添字となる数値を得る
-      #もしもwday_numが7以上であれば、7を引く
       if wday_num <= 7
         wday_num = wday_num - 7
       end
